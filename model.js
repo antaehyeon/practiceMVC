1fb17e5850c218ccce37659d37c4bbaf1ba3f08d
jisuyoun <nigayoun@gmail.com>
2018-07-18 14:08:36 +0900

    review. 코드리뷰 남깁니다

--- model.js
+++ model.js
@@ -1,6 +1,13 @@
 class TodoModel {
 
     constructor() {
+        /* 
+        model이라는 것이 DOM node를 말하는 게 아닙니다.(절대로)
+        model의 데이터는 DOM에 있는 것을 찾는게 아니고, 자료구조로 가지고 있어야 합니다.
+        당장화면에 보여지지 않는 데이터까지 모두 다 화면에 존재하지 않을테니까요.
+        이런식으로요
+        this.todolist = [];
+        */
         this.todoInputBox = document.getElementsByName("todo")[0];
         this.todoRegisterationBtn = document.getElementsByTagName("button");
         this.todoListParentUlTag = document.getElementsByClassName("todolist")[0];
--- view.js
+++ view.js
@@ -1,5 +1,8 @@
+//네. view는 진짜 렌더링에 집중하는 경우가 많아요.
 class TodoView {
     
+    //지금은 controller 를 통해서 데이터를 받아서 화면렌더링을 하는 코드죠.
+    //괜찮습니다. 그런데 view에서 model을 접근해서 가져오기도 해요.
     registerTask(parentNode, childNode) {
         parentNode.appendChild(childNode);
     }
